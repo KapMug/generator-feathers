5511af90f0c7bd907ef2e9bedfbf25e31a73dea6
joao_at_avido <joao@avi.do>
2017-03-07 19:18:55 -0300

    fix config

--- generators/connection/templates/couchdb.js
+++ generators/connection/templates/couchdb.js
@@ -6,6 +6,7 @@ const service = require('feathers-couchdb');
 module.exports = function() {
   const app = this;
   const config = app.get('couchdb');
+  const db_auth = app.get('couchdb_auth')
   
 
 //var Connection = new(cradle.Connection)('http://192.168.1.79:5984')
@@ -13,8 +14,8 @@ const Connection = new(cradle.Connection)(config,
     {
         secure: false,
          auth: {
-             username: config.dbUsername,
-             password: config.dbPassword
+             username: db_auth.dbUsername,
+             password: db_auth.dbPassword
         },
         cache: true
     }
